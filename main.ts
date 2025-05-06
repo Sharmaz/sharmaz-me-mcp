3a29676c0f12fbc775311ecb1d5c087ab1dd775a
Ivan Robles <irae45@gmail.com>
2025-05-06 16:32:18 -0600

    Fetch the portfolio data at fetch-portfolio tool.

--- main.ts
+++ main.ts
@@ -7,18 +7,50 @@ const server = new McpServer({
   version: "0.1.0",
 });
 
+if (!process.env.API_KEY || !process.env.API_URL || !process.env.USER_ID) {
+  console.error('API_KEY, API_URL, and USER_ID are required but not set in the environment variables.');
+  process.exit(1);
+}
+
+const config = {
+  apiKey: process.env.API_KEY,
+  apiUrl: process.env.API_URL,
+  userId: process.env.USER_ID,
+};
+
 server.tool(
   'fetch-portfolio',
   'Tool to fetch the portfolio information from the Sharmaz API',
   {
-    sideProjectName: z.string().describe("The side project name"),
+    portfolio: z.string().describe("All elements at the portfolio"),
   },
-  async ({ sideProjectName }) => {
+  async ({ portfolio }) => {
+    const data = await fetch(`${config.apiUrl}/api/v1/users/${config.userId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        api: config.apiKey || '',
+      },
+    });
+
+    const portfolioData = await data.json();
+
+    if (portfolioData.length === 0) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: `The portfolio ${portfolio} not found`
+          }
+        ]
+      }
+    }
+
     return {
       content: [
         {
           type: "text",
-          text: `The project ${sideProjectName} is a side project of Sharmaz. It is a portfolio project that showcases the skills and expertise of the developer.`
+          text: JSON.stringify(portfolioData, null, 2)
         }
       ]
     }
